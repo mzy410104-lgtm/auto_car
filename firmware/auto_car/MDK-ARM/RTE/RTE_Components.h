12039bef2259d8bb3dc74c09382b6098de6f3528
mzy410104 <mzy410104@gmail.com>
2026-09-22 09:42:40 +0200

    Add STM32 UART communication baseline

---
+++ firmware/auto_car/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'auto_car' 
+ * Target:  'auto_car' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
